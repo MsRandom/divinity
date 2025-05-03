9a5980dfa58eb2de764b1b8fdd052aa7b347be3a
Ashley Wright <ashley@msrandom.net>
2025-05-04 00:13:52 +0300

    Fix incorrect block entity used

--- src/main/kotlin/net/msrandom/divinity/world/level/block/BlowMoldBlock.kt
+++ src/main/kotlin/net/msrandom/divinity/world/level/block/BlowMoldBlock.kt
@@ -6,9 +6,10 @@ import net.minecraft.world.level.block.BaseEntityBlock
 import net.minecraft.world.level.block.RenderShape
 import net.minecraft.world.level.block.state.BlockState
 import net.msrandom.divinity.world.level.block.entity.BellowsBlockEntity
+import net.msrandom.divinity.world.level.block.entity.BlowMoldBlockEntity
 
 class BlowMoldBlock(properties: Properties) : BaseEntityBlock(properties) {
     override fun getRenderShape(state: BlockState) = RenderShape.ENTITYBLOCK_ANIMATED
     override fun codec(): MapCodec<BlowMoldBlock> = simpleCodec(::BlowMoldBlock)
-    override fun newBlockEntity(pos: BlockPos, state: BlockState) = BellowsBlockEntity(pos, state)
+    override fun newBlockEntity(pos: BlockPos, state: BlockState) = BlowMoldBlockEntity(pos, state)
 }
