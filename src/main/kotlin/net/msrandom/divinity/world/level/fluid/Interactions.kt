83fbdf57ca338578ecc51847534fa60362096a4b
Ashley Wright <ashley@msrandom.net>
2025-05-04 02:40:31 +0300

    Fix fluid interaction block placing

--- src/main/kotlin/net/msrandom/divinity/world/level/fluid/Interactions.kt
+++ src/main/kotlin/net/msrandom/divinity/world/level/fluid/Interactions.kt
@@ -2,8 +2,10 @@ package net.msrandom.divinity.world.level.fluid
 
 import net.minecraft.core.Direction
 import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.LevelEvent
 import net.minecraft.world.level.block.state.properties.BlockStateProperties
 import net.neoforged.neoforge.common.NeoForgeMod
+import net.neoforged.neoforge.event.EventHooks
 import net.neoforged.neoforge.fluids.FluidInteractionRegistry
 
 fun getMoltenFluidInteraction(solidForm: () -> Block) = FluidInteractionRegistry.InteractionInformation(
@@ -15,7 +17,7 @@ fun getMoltenFluidInteraction(solidForm: () -> Block) = FluidInteractionRegistry
     { level, currentPos, relativePos, state ->
         val block = solidForm()
 
-        if (BlockStateProperties.HORIZONTAL_FACING in block.stateDefinition.properties) {
+        val newState = if (BlockStateProperties.HORIZONTAL_FACING in block.stateDefinition.properties) {
             block.defaultBlockState().setValue(
                 BlockStateProperties.HORIZONTAL_FACING,
                 Direction.from2DDataValue(currentPos.asLong().toInt()),
@@ -23,5 +25,8 @@ fun getMoltenFluidInteraction(solidForm: () -> Block) = FluidInteractionRegistry
         } else {
             block.defaultBlockState()
         }
+
+        level.setBlockAndUpdate(currentPos, EventHooks.fireFluidPlaceBlockEvent(level, currentPos, currentPos, newState));
+        level.levelEvent(LevelEvent.LAVA_FIZZ, currentPos, 0);
     },
 )
